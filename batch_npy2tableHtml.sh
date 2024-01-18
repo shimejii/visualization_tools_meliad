7fc6aae85e938994724c432997864b8337c19930
hiraguchi <shimeshimejiji0@gmail.com>
2024-01-18 20:15:09 +0900

    update: update so that three-dimensional lists can also be converted as html tables

--- batch_npy2tableHtml.sh
+++ batch_npy2tableHtml.sh
@@ -13,6 +13,7 @@ source "$1"
 # END_IDX_MEMORY=1  # 任意の終了メモリインデックスを指定
 # IDC_DEVICE=("0" "1")
 
+# for 2d array
 for exec_mode in ${EXEC_MODES[@]}  # 任意の実行モードを指定
 do
   for database_name in ${DATABASE_NAMES[@]}  # 任意のデータベース名を指定
@@ -30,3 +31,30 @@ do
     done
   done
 done
+
+
+# for 3d array
+for exec_mode in ${EXEC_MODES[@]}  # 任意の実行モードを指定
+do
+  for i in {1..${#DATABASE_NAMES_3DARRAY[@]}}  # 任意のデータベース名を指定
+  do
+    # echo ${i}
+    database_name=${DATABASE_NAMES_3DARRAY[${i}]}
+    # echo ${database_name}
+    for step in {${START_STEP}..${END_STEP}}
+    do
+      for idx_memory in {${START_IDX_MEMORY}..${END_IDX_MEMORY}}
+      do
+        for idx_device in ${IDC_DEVICE[@]}
+        do
+          for idx_3dim in {$EXPORT_RANGE_START_FOR_3D_DATABASE[${i}]..$EXPORT_RANGE_END_FOR_3D_DATABASE[${i}]}
+          do
+            table_name="mode-${exec_mode}_dbname-${database_name}_idx3dim-${idx_3dim}_step-${step}_memoryIndex-${idx_memory}_deviceIdx-${idx_device}"
+            python main.py npy2tablehtml "$WORKDIR" "$exec_mode" "$database_name" "$step" "$idx_memory" "$idx_device" "$table_name" --dim3_idx "$idx_3dim" > ${WORKDIR}/${table_name}.html
+          done
+        done
+      done
+    done
+  done
+done
+
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import argparse
 import sys
 import utils
+import jax.numpy as jnp
 
 def npy2tablehtml(args):
     arr = utils.load_exported_db_from_npy(
         args.workdir, args.exec_mode, args.database_name, args.step, args.idx_memory)
     arr = arr[args.idx_device]
-    if len(arr.shape) == 3:
-        arr = arr[:,:,0]
-    tableHTML = utils.ndarray2htmlTable(arr)
-    name = "<p>" + args.table_name_for_html + "</p>\n"
-    print(name + tableHTML)
+    arr = jnp.array(arr)
+    if len(arr.shape) == 1:
+        tableHTML = utils.ndarray2htmlTable(arr.reshape(1, arr.shape[0]))
+        name = "<p>" + args.table_name_for_html + "</p>\n"
+        print(name + tableHTML)
+    elif len(arr.shape) == 2:
+        tableHTML = utils.ndarray2htmlTable(arr)
+        name = "<p>" + args.table_name_for_html + "</p>\n"
+        print(name + tableHTML)
+    elif len(arr.shape) == 3:
+        assert 0 <= args.dim3_idx and args.dim3_idx < arr.shape[2]
+        tableHTML = utils.ndarray2htmlTable(arr[:,:,args.dim3_idx])
+        name = "<p>" + args.table_name_for_html + "</p>\n"
+        print(name + tableHTML)
 
 # argv = sys.argv
 parser = argparse.ArgumentParser()
@@ -25,6 +35,7 @@ parser_npy2tablehtml.add_argument("step", help="Target number of steps", type=in
 parser_npy2tablehtml.add_argument("idx_memory", help="Index of external memory", type=int)
 parser_npy2tablehtml.add_argument("idx_device", help="Index of device", type=int)
 parser_npy2tablehtml.add_argument("table_name_for_html", help="Name of the table to use for labeling when outputting to html")
+parser_npy2tablehtml.add_argument("--dim3_idx", help="Target 3D  index", type=int)
 parser_npy2tablehtml.set_defaults(handler=npy2tablehtml)
 
 args = parser.parse_args()
